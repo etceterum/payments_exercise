e119824bb56df82fa43708e70d66491ec2d123a3
Evgeny Karpov <ekarpov@gmail.com>
2015-10-30 17:23:42 -0500

    Notes on missing features

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,2 +1,9 @@
 class Loan < ActiveRecord::Base
+  has_many :payments, dependent: :destroy
+  # since it is outside the scope of the assignment, 
+  # not adding any validations for Loan - normally, one should
+
+  def outstanding_balance
+    funded_amount - payments.sum(:amount)
+  end
 end
--- db/migrate/20150903195334_create_loans.rb
+++ db/migrate/20150903195334_create_loans.rb
@@ -1,6 +1,9 @@
 class CreateLoans < ActiveRecord::Migration
   def change
     create_table :loans do |t|
+      # since this is not part of the assignment, not fixing the below:
+      # normally, one would enforce funded_amount to be non-null on both
+      # the database level (here) and model level (Loan class)
       t.decimal :funded_amount, precision: 8, scale: 2
       t.timestamps null: false
     end
